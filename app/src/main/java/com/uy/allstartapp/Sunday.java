223c001a42eca9cb6aaa77b99956d0c8611e9dfe
Krisha Uy <2014069077@ust-ics.mygbiz.com>
2017-09-20 22:40:17 +0800

    Initial commit

---
+++ app/src/main/java/com/uy/allstartapp/Sunday.java
@@ -0,0 +1,13 @@
+package com.uy.allstartapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sunday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sunday);
+    }
+}
